1e53dfc10c0ee4c4a165fa420c33fa38137f53ef
Kunal30031998 <38662155+Kunal30031998@users.noreply.github.com>
2026-06-16 21:32:44 +0530

    Create HealthcareApiApplication class for Spring Boot

---
+++ src/main/java/com/deepthi/healthcareapi/HealthcareApiApplication.java
@@ -0,0 +1,11 @@
+package com.deepthi.healthcareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareApiApplication.class, args);
+    }
+}
