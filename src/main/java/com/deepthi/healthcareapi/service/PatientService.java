5f85afb36b2b7672384549f314a65f72f75b6007
Kunal30031998 <38662155+Kunal30031998@users.noreply.github.com>
2026-06-16 21:36:19 +0530

    Implement PatientService for managing patients

---
+++ src/main/java/com/deepthi/healthcareapi/service/PatientService.java
@@ -0,0 +1,24 @@
+package com.deepthi.healthcareapi.service;
+
+import com.deepthi.healthcareapi.model.Patient;
+import com.deepthi.healthcareapi.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.getAllPatients();
+    }
+
+    // Homework method
+    public Optional<Patient> getPatientById(int id) {
+        return patientRepository.getPatientById(id);
+    }
+}
